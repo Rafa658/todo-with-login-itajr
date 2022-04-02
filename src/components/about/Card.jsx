1d55675bb0d9266dd9c43fcec432e267756a9045
Rafael <rafollo@live.com>
2022-04-02 17:40:15 -0300

    Adicionada seção To-Do List

--- src/components/about/Card.jsx
+++ src/components/about/Card.jsx
@@ -8,7 +8,7 @@ const Card = () => {
         <div className="Card">
             <img src={Rafael} alt='Rafael' />
             <span className="header">Rafael</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum temporibus ipsa quas, fugit natus quo impedit tempora illo itaque ipsum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur possimus veritatis autem beatae a architecto quidem minus aliquam. Earum, quaerat amet, a neque deleniti reiciendis facere vitae non enim, voluptas itaque nostrum magnam cumque labore quasi dignissimos porro ratione hic aliquid nobis exercitationem? Eveniet repellat incidunt omnis nemo explicabo id.</p>
         </div>
     )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImPlus, ImCross } from 'react-icons/im'
+import './Home.css'
 
 const Home = () => {
 
+    const [input, setInput] = useState('')
+    const [count, setCount] = useState(0)
+    const [chores, setChores] = useState([])
+
     return (
-        <div>
-            Home
+        <div className="Home">
+            <span className="text header">Lista de Afazeres</span>
+            <div className="list">
+                <div className="container">
+                    <input
+                        type="text"
+                        placeholder="Insira nova tarefa"
+                        value={input}
+                        onChange={e => {
+                            setInput(e.target.value)
+                        }}
+                    />
+                    <div
+                        type='submit'
+                        className="addBtn"
+                        onClick={input ? e => {
+                            e.preventDefault(); //evita que a página recarregue
+
+                            setCount(count + 1)
+
+                            let newChore = {
+                                index: count,
+                                desc: input
+                            }
+
+                            setChores(current => [...current, newChore])
+                            setInput('')
+                        } : null}
+                    >
+                        <ImPlus />
+                    </div>
+                </div>
+                {chores.map(chore => {
+                    return (
+                        <li key={chore.index} className='container chore-item'> {chore.index + 1}: {chore.desc} </li>
+                    )
+                })}
+            </div>
         </div>
     )
 }
